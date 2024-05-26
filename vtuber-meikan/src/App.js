8f8a231eb208d48b631c4dbdb4189a6192fb6cfc
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2024-05-26 18:29:24 +0900

    試しに一部分のコードを置き換え実施

--- vtuber-meikan/src/App.js
+++ vtuber-meikan/src/App.js
@@ -2,22 +2,88 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="MainContents noteWithShadow">
+      <h2>トップページ</h2>
+      <div>Vtuber名鑑とは</div>
+      <div class="PunchHole"></div>
+      <div class="PunchHole"></div>
+      <div class="PunchHole"></div>
+      <div>　ああああああ</div>
+      <div>　あああああ</div>
+      <div>　あああああああああああ</div>
+      <div>最近の更新</div>
+      <div>2024/2/24更新！</div>
+      <div class="card-group">
+        <div class="card">
+          <div class="overviewTextArea">
+            <div class="overviewTitle">
+              タイトルがはいります。タイトルがはいります。
+            </div>
+            <div class="overviewText">
+              概要がはいります。概要がはいります。概要がはいります。概要がはいります。
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="overviewTextArea">
+            <div class="overviewTitle">
+              タイトルがはいります。タイトルがはいります。
+            </div>
+            <div class="overviewText">
+              概要がはいります。概要がはいります。概要がはいります。概要がはいります。
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="overviewTextArea">
+            <div class="overviewTitle">なNaシﾉ　権β-∀1</div>
+            <div class="overviewText">誕生日</div>
+            <div class="overviewText">箱（所属事務所）</div>
+            <div class="overviewText">youtubeリンクアイコンなど</div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="overviewTextArea">
+            <div class="overviewTitle">なNaシﾉ　権β-∀2</div>
+            <div class="overviewText">誕生日</div>
+            <div class="overviewText">箱（所属事務所）</div>
+            <div class="overviewText">youtubeリンクアイコンなど</div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="overviewTextArea">
+            <div class="overviewTitle">なNaシﾉ　権β-∀3</div>
+            <div class="overviewText">誕生日</div>
+            <div class="overviewText">箱（所属事務所）</div>
+            <div class="overviewText">youtubeリンクアイコンなど</div>
+          </div>
+        </div>
+      </div>
+      <div>新人Vtuber</div>
+      <div>2024/2/24更新！　XXXXXXX</div>
+      <div>2024/2/24更新！　XXXXXXX</div>
+      <div>よく見られているVtuber</div>
+      <div>XXXXXXX</div>
+      <div>XXXXXXX</div>
     </div>
   );
 }
