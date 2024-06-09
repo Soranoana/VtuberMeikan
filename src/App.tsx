abdfb66866d88e6c91e04ec7cb82cf3b8134ecf3
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2024-06-10 03:33:52 +0900

    ts化に成功（M1 Mac book環境）

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import logo from './logo.svg';
+import './App.css';
+// import './testCss.css'
+
+function App() {
+  // return (
+  //   <div classNameName="App">
+  //     <header classNameName="App-header">
+  //       <img src={logo} classNameName="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         classNameName="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  return (
+    <>
+        <div className="MainContents noteWithShadow">
+      <h2>トップページ</h2>
+      <div>Vtuber名鑑とは</div>
+      <div className="PunchHole"></div>
+      <div className="PunchHole"></div>
+      <div className="PunchHole"></div>
+      <div>　ああああああ</div>
+      <div>　あああああ</div>
+      <div>　あああああああああああ</div>
+      <div>最近の更新</div>
+      <div>2024/2/24更新！</div>
+      <div className="card-group">
+        <div className="card">
+          <div className="overviewTextArea">
+            <div className="overviewTitle">
+              タイトルがはいります。タイトルがはいります。
+            </div>
+            <div className="overviewText">
+              概要がはいります。概要がはいります。概要がはいります。概要がはいります。
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="overviewTextArea">
+            <div className="overviewTitle">
+              タイトルがはいります。タイトルがはいります。
+            </div>
+            <div className="overviewText">
+              概要がはいります。概要がはいります。概要がはいります。概要がはいります。
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="overviewTextArea">
+            <div className="overviewTitle">なNaシﾉ　権β-∀1</div>
+            <div className="overviewText">誕生日</div>
+            <div className="overviewText">箱（所属事務所）</div>
+            <div className="overviewText">youtubeリンクアイコンなど</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="overviewTextArea">
+            <div className="overviewTitle">なNaシﾉ　権β-∀2</div>
+            <div className="overviewText">誕生日</div>
+            <div className="overviewText">箱（所属事務所）</div>
+            <div className="overviewText">youtubeリンクアイコンなど</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="overviewTextArea">
+            <div className="overviewTitle">なNaシﾉ　権β-∀3</div>
+            <div className="overviewText">誕生日</div>
+            <div className="overviewText">箱（所属事務所）</div>
+            <div className="overviewText">youtubeリンクアイコンなど</div>
+          </div>
+        </div>
+      </div>
+      <div>新人Vtuber</div>
+      <div>2024/2/24更新！　XXXXXXX</div>
+      <div>2024/2/24更新！　XXXXXXX</div>
+      <div>よく見られているVtuber</div>
+      <div>XXXXXXX</div>
+      <div>XXXXXXX</div>
+    </div>
+    </>
+
+  );
+}
+
+export default App;
