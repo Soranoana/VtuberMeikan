c18d83dfde85ca3f6d394cb2aaa0a2df9658b339
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2024-06-16 21:24:56 +0900

    基本のページ構成を作成

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,5 +1,30 @@
+import React from 'react';
+import PageTitle from "./common/PageTitle";
+import NavigationMenu from "./common/NavigationMenu";
+import LeftContent from "./common/LeftContent";
+import ViewFooter from "./common/ViewFooter";
+import SiteFooter from "./common/SiteFooter";
+import Top from "./top";
+
 export default function page() {
     return (
-        <>Hello React World!!</>
+        <>
+            <body>
+                <PageTitle />
+                <NavigationMenu />
+                <LeftContent />
+                <RightContent />
+                <ViewFooter />
+                <SiteFooter />
+            </body>
+        </>
+    )
+}
+
+function RightContent() {
+    return (
+        <>
+            <Top />
+        </>
     )
 }
