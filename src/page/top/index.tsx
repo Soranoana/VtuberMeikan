1b2654e4d29d28db449e39ba4524d4f8014eda9a
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2024-06-18 23:54:47 +0900

    トップ画面とタイトル画面のコードが逆になっていたため修正

--- src/page/top/index.tsx
+++ src/page/top/index.tsx
@@ -3,10 +3,145 @@ import "./../../testCss.css";
 
 export default function page() {
     return (
-        <div className="PageTitle">
-            <div className="TitleText">
-                <h1>VTuberプロフィール帳</h1>
+        <>
+            <div className="RightContent">
+                <div className="MainContents noteWithShadow">
+                    <h2>トップページ</h2>
+                    {/* <p>右側サイトコンテンツ</p> */}
+                    <div>Vtuber名鑑とは</div>
+                    <div className="PunchHole"></div>
+                    <div className="PunchHole"></div>
+                    <div className="PunchHole"></div>
+                    <img className="PunchHoleImg" src="punchHole.png" alt="" /><br />
+                    <img className="PunchHoleImg" src="punchHole.png" alt="" /><br />
+                    <img className="PunchHoleImg" src="punchHole.png" alt="" /><br />
+                    <div>　ああああああ</div>
+                    <div>　あああああ</div>
+                    <div>　あああああああああああ</div>
+                    <div>最近の更新</div>
+                    <div>2024/2/24更新！</div>
+                    <div className="card-group">
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">
+                                    タイトルがはいります。タイトルがはいります。
+                                </div>
+                                <div className="overviewText">
+                                    概要がはいります。概要がはいります。概要がはいります。概要がはいります。
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">
+                                    タイトルがはいります。タイトルがはいります。
+                                </div>
+                                <div className="overviewText">
+                                    概要がはいります。概要がはいります。概要がはいります。概要がはいります。
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀1</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀2</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀3</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀4</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀5</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀6</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀7</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀8</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀9</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="Vtuber顔1.png" alt="" className="overviewImg" />
+                            <div className="overviewTextArea">
+                                <div className="overviewTitle">なNaシﾉ　権β-∀10</div>
+                                <div className="overviewText">誕生日</div>
+                                <div className="overviewText">箱（所属事務所）</div>
+                                <div className="overviewText">youtubeリンクアイコンなど</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>新人Vtuber</div>
+                    <div>2024/2/24更新！　XXXXXXX</div>
+                    <div>2024/2/24更新！　XXXXXXX</div>
+                    <div>よく見られているVtuber</div>
+                    <div>XXXXXXX</div>
+                    <div>XXXXXXX</div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
